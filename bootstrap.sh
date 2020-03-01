943ee79cc546d56b85b3dfef75b540b163c4c209
bobman743 <4vm2j1bbl8zl05qp@gmail.com>
2020-03-01 11:44:42 -0800

    Fix some shit

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,14 @@
 
 set -euxo pipefail
 
-declare -r salt_version=3003
+# We assume tumbleweed
+sudo zypper in lsb-release python3-dev
+
+declare -r salt_version=3000
 declare -r salt_hash=efc46700aca78b8e51d7af9b06293f52ad495f3a8179c6bfb21a8c97ee41f1b7
 declare -r salt_filename=bootstrap-salt.sh
 
 curl -o "${salt_filename}" -L https://bootstrap.saltstack.com
 echo "${salt_hash} ${salt_filename}" | sha256sum --check
 chmod +x "${salt_filename}"
-exec "${salt_filename}" -N -x python3 stable "${salt_version}"
+sudo exec ./"${salt_filename}" -X -x python3 git "${salt_version}"
