0c06c0329bbdc0b7c2d0cee74a1375b310c7f2b5
bobman743 <4vm2j1bbl8zl05qp@gmail.com>
2020-03-01 11:15:30 -0800

    test

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,9 @@
 set -euxo pipefail
 
 declare -r salt_version=3003
+declare -r salt_hash=efc46700aca78b8e51d7af9b06293f52ad495f3a8179c6bfb21a8c97ee41f1b7
+declare -r salt_filename=bootstrap-salt.sh
 
-curl -o bootstrap-salt.sh -L https://bootstrap.saltstack.com
-sha256sum bootstrap-salt.sh
+curl -o "${salt_filename}" -L https://bootstrap.saltstack.com
+echo "${salt_hash} ${salt_filename}" | sha256sum --check
+# if [ $? -ne 0 ]; then
