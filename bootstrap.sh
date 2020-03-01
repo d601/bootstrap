68e0b82f75b64e6c5c89e84868e07e189a29e036
bobman743 <4vm2j1bbl8zl05qp@gmail.com>
2020-03-01 12:00:08 -0800

    Install gcloud

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,13 @@ set -euxo pipefail
 # We assume tumbleweed
 sudo zypper in -y lsb-release python3-devel
 
+# Install gcloud
+# I don't think they publish hashes for this
+curl https://sdk.cloud.google.com > gcloud_installer
+chmod +x gcloud_installer
+exec gcloud_installer
+
+# Install salt
 declare -r salt_version=3000
 declare -r salt_hash=efc46700aca78b8e51d7af9b06293f52ad495f3a8179c6bfb21a8c97ee41f1b7
 declare -r salt_filename=bootstrap-salt.sh
